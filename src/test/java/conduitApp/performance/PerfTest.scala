da1a5c91dd00ef1e8311deaebfab2cbcbbc16604
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-07-15 19:33:06 -0700

    #41 custom feeder

--- src/test/java/conduitApp/performance/PerfTest.scala
+++ src/test/java/conduitApp/performance/PerfTest.scala
@@ -4,17 +4,25 @@ import com.intuit.karate.gatling.PreDef._
 import io.gatling.core.Predef._
 import scala.concurrent.duration._
 
+import conduitApp.performance.createTokens.CreateTokens
+
 class PerfTest extends Simulation {
+
+    CreateTokens.createAccessTokens()
     
     val protocol = karateProtocol(
         "/api/articles/{articleId}" -> Nil
     )
+
+    protocol.nameResolver = (req, ctx) => req.getHeader("karate-name")
     
     val csvFeeder = csv("articles.csv").circular
-//       val tokenFeeder = Iterator.continually(Map("token" -> CreateTokens.getNextToken))   
-//       .feed(tokenFeeder)
+    val tokenFeeder = Iterator.continually(Map("token" -> CreateTokens.getNextToken))   
 
-    val createArticle = scenario("Create & Delete Article").feed(csvFeeder).exec(karateFeature("classpath:conduitApp/performance/createArticle.feature"))
+    val createArticle = scenario("Create & Delete Article")
+        .feed(csvFeeder)
+        .feed(tokenFeeder)
+        .exec(karateFeature("classpath:conduitApp/performance/createArticle.feature"))
         
     setUp(
         createArticle.inject(
@@ -29,11 +37,11 @@ class PerfTest extends Simulation {
         )
 }
 
-// import conduitApp.performance.createTokens.CreateTokens
 
-// CreateTokens.createAccessTokens()
 
-//   protocol.nameResolver = (req, ctx) => req.getHeader("karate-name")
+
+
+  
 
 
 
--- src/test/java/conduitApp/performance/createArticle.feature
+++ src/test/java/conduitApp/performance/createArticle.feature
@@ -5,22 +5,22 @@ Feature: Article
         * def dataGenerator = Java.type('helpers.DataGenerator')
         * set articleRequestBody.article.title = __gatling.Title       // #40 change at
         * set articleRequestBody.article.description = __gatling.Description  // #40 change at
-        * def articleRequestBody.article.body = dataGenerator.getRandomArticleValues().body
+        * set articleRequestBody.article.body = dataGenerator.getRandomArticleValues().body
 
         * def sleep = function(ms){ java.lang.Thread.sleep(ms) }
         * def pause = karate.get('__gatling.pause', sleep)
 
     Scenario: Create & Delete Article
-        # * configure headers = {"Authorization": #('Token ' + __gatling.token)}
+        * configure headers = {"Authorization": #('Token ' + __gatling.token)}
         Given path 'articles'
         And request articleRequestBody
         # And header karate-name = 'Create Article'
         When method Post
-        Then status 200
+        Then status 201
         * def articleId = response.article.slug
 
         # * pause(5000)                     // #40 commenting temp
 
         # Given path 'articles',articleId   // #40 commenting temp
         # When method Delete
-        # Then status 200
+        # Then status 204
